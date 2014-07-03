0000bb9d46fad0fc64d7610e54a99b2fe50b247b
Tommaso Papini <tommaso.papini@cern.ch>
2014-07-03 10:02:33 +0200

    Second version

--- ccustomization/static/js/edit.js
+++ ccustomization/static/js/edit.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
-    var widget = $('#widget-test');
-    widget.width(widget.width()+2);
-    widget.height(widget.height()+1);
-    widget.draggable({
-        grid: [widget.width(), widget.height()],
-        containment: '.modification-grid',
-        scroll: false
-    }).resizable({
-        grid: [widget.width(), widget.height()]
-    });
+    function enableDrag(elem, grid) {
+        elem.draggable({
+            grid: grid,
+            containment: '.modification-grid',
+            scroll: false
+        }).resizable({
+            grid: grid,
+            containment: '.modification-grid'
+        }).addClass('active').on('click', function(){
+            var select = true;
+            if (elem.hasClass('selected')) {
+                select = false;
+            }
+            $('.widget-test').removeClass('selected');
+            elem.toggleClass('selected', select);
+        });
+    }
+
+    var widget = $('.widget-test');
+    var width = $('.modification-grid .grid-row .grid-cell').width();
+    var height = $('.modification-grid .grid-row .grid-cell').height();
+    widget.width(width-8);
+    widget.height(height-8);
+    var grid = [width+1, height+1];
+    enableDrag($('#widget-0-0'), grid);
+    enableDrag($('#widget-1-2'), grid);
 });
