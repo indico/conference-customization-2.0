3ace2ec69f696719593355ca8b1a11000ac53bbf
Tommaso Papini <tommaso.papini@cern.ch>
2014-07-07 14:15:14 +0200

    Container: second version

--- ccustomization/static/js/edit.js
+++ ccustomization/static/js/edit.js
@@ -21,48 +21,73 @@ $(document).ready(function() {
         handle: '.ui-icon.ui-icon-arrow-4'
     });
 
-    var widgetCount = 0,
-        containerCount = 0;
+    var widgetCount = 0;
 
     $('.widget-button button').on('click', function(){
         var widget = $('<li>', {
-            class: 'widget-test',
+            'class': 'widget-test',
             text: 'Widget '+widgetCount
         });
         var drag = $('<span>', {
-            class: 'widget-drag ui-icon ui-icon-arrow-4'
+            'class': 'ui-icon ui-icon-arrow-4'
+        });
+        var container = $('<li>', {
+            'class': 'container-test'
+        });
+        var containerList = $('<ul>', {
+            'class': 'container-list'
         });
         drag.appendTo(widget);
         widget.on('click', function(){
-            var select = true;
-            if (widget.hasClass('selected')) {
-                select = false;
-            }
+            var select = ! widget.hasClass('selected');
             $('.widget-test').removeClass('selected');
             widget.toggleClass('selected', select);
-        }).appendTo($('#column-0-container .column-list'));
-        widgetCount++;
-    });
-
-    $('.container-button button').on('click', function(){
-        var container = $('<li>', {
-            class: 'container-test',
-            id: 'container-'+containerCount
-        });
-        var drag = $('<span>', {
-            class: 'container-drag ui-icon ui-icon-arrow-4'
-        });
-        var containerList = $('<ul>', {
-            class: 'container-list'
-        });
-        drag.appendTo(container);
+        }).appendTo(containerList);
         containerList.appendTo(container);
         container.appendTo($('#column-0-container .column-list'));
-        containerCount++;
+        widgetCount++;
 
         containerList.sortable({
             connectWith: '.column-list, .container-list',
             handle: '.ui-icon.ui-icon-arrow-4'
+        });
+    });
+
+    $('body').on('sortstop', function(){
+        $('.container-test').each(function(){
+            $this = $(this);
+            var list = $this.children('.container-list');
+            if (list.children().length < 1) {
+                $this.remove();
+            } else if (list.children().length > 1 && $this.children('.ui-icon.ui-icon-arrow-4').length < 1) {
+                list.detach();
+                var drag = $('<span>', {
+                    'class': 'ui-icon ui-icon-arrow-4'
+                });
+                drag.appendTo($this);
+                list.appendTo($this);
+                $this.addClass('filled');
+            } else if (list.children().length == 1) {
+                $this.children('.ui-icon.ui-icon-arrow-4').remove();
+                $this.removeClass('filled');
+            }
+        });
+        $('.widget-test').each(function(){
+            var $this = $(this);
+            if (!$this.parent().hasClass('container-list')) {
+                var container = $('<li>', {
+                    'class': 'container-test'
+                });
+                var containerList = $('<ul>', {
+                    'class': 'container-list'
+                });
+                $this.wrap(containerList);
+                $this.parent().wrap(container);
+                $this.parent().sortable({
+                    connectWith: '.column-list, .container-list',
+                    handle: '.ui-icon.ui-icon-arrow-4'
+                });
+            }
         });
     });
 
