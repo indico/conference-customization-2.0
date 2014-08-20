96036016d57ea18a09483faa6d995ce96de0ebc8
Tommaso Papini <tommaso.papini@cern.ch>
2014-08-20 15:37:14 +0200

    Sortable placeholders

--- ccustomization/static/js/main.js
+++ ccustomization/static/js/main.js
@@ -1,4 +1,6 @@
 var sortableOptions = {
     connectWith: '.column-list, .container-list',
-    handle: '.ui-icon.ui-icon-arrow-4'
+    handle: '.ui-icon.ui-icon-arrow-4',
+    placeholder: "sortable-placeholder",
+    forcePlaceholderSize: true
 };
